0611317a10201e59dfcb97af947bea22c021c135
Sourav Saha <souravsaha111000@gmail.com>
2024-07-28 05:45:39 +0530

    pasted

--- scripts/word_guessing_game.py
+++ scripts/word_guessing_game.py
@@ -1,1 +1,40 @@
-dfd
+#!/usr/bin/env python3
+
+word = input('Please enter a word for your opponent: ')
+
+# Initialize variables
+wrongGuesses = 7
+guessed = False
+correctLetters = ''
+wrongLetters = ''
+blanks = ''
+
+for letter in word:
+    blanks = blanks+'_ '
+
+print(blanks)
+
+print('You have', wrongGuesses, 'wrong guesses left.')
+
+while guessed is False and wrongGuesses > 0:
+
+    print('Correct Letters Guessed: ', correctLetters)
+    print('Wrong Letters Guessed: ', wrongLetters)
+
+    guess = input('What letter would you like to guess?')
+
+    blanks = ''
+    validLetter = False
+
+    for letter in word:
+        if letter == guess:
+            validLetter = True
+
+    if validLetter == True:
+        correctLetters = correctLetters + guess
+        print('Correct!', 'The letter', guess, 'is in the word.')
+    else:
+        wrongLetters = wrongLetters + guess
+        wrongGuesses = wrongGuesses - 1
+        print("I'm sorry,", guess, "is not in the word.")
+        print('You have', wrongGuesses, 'wrong guesses left.')
